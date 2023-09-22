984ea2ab1bff03ce42a82512e026f8a433913b2e
Mario Zechner <badlogicgames@gmail.com>
2023-09-22 22:26:46 +0200

    Trigger mastoreader Docker image reload on publish.

--- publish.sh
+++ publish.sh
@@ -7,3 +7,5 @@ rsync -avz ./site/ badlogic@marioslab.io:/home/badlogic/mastoreader.io/data/web/
 source_file="/home/badlogic/mastoreader.io/data/web/mastoreader.html"
 destination_file="/home/badlogic/mastoreader.io/data/web/index.html"
 scp "marioslab.io:$source_file" "marioslab.io:$destination_file"
+
+ssh -t marioslab.io "cd mastoreader.io && ./reload.sh"
